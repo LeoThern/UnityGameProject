fe8e0f20cb2b7f3c5a3c6271f4c08eff1c031685
Kristian Rehn <kristian.rehn@stud.uni-hannover.de>
2024-01-16 15:06:18 +0100

    Add MainMenu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+
+    public void openOptions()
+    {
+
+    }
+
+    public void openCredits()
+    {
+
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+  
+}
