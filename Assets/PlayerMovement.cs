2ca4e9e78d53b60343770c81ee64cb860eda3691
LeoThern <47295238+LeoThern@users.noreply.github.com>
2024-01-21 15:37:24 +0100

    2player movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class PlayerMovement : MonoBehaviour
     private bool doesWeakAttack = false;
     private bool doesStrongAttack = false;
     private bool doesEvade = false;
+    private Vector2 movementInput = Vector2.zero;
 
     [SerializeField] public bool playerId = false;
     [SerializeField] private float jumpingPower = 8f;
@@ -32,8 +34,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-
         CheckOnGround();
         CheckJump();
         CheckFlip();
@@ -42,9 +42,15 @@ public class PlayerMovement : MonoBehaviour
         CheckStrongAttack();
     }
 
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+        horizontal = movementInput.x;
+    }
+
     private void FixedUpdate()
     {
-        horizontal = doesStrongAttack ? 0f : Input.GetAxisRaw("Horizontal");
+        horizontal = doesStrongAttack ? 0f : horizontal;
         Vector2 movement = new Vector2(speed * horizontal, rb.velocity.y);
         rb.velocity = Vector2.Lerp(rb.velocity, movement, lerpConstant);
         animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
