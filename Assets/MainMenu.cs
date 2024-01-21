d9033c8fda44ba5e31785974ce0e753f7d550c85
Kristian Rehn <kristian.rehn@stud.uni-hannover.de>
2024-01-21 17:08:37 +0100

    add Creditsscene

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -7,23 +7,24 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadSceneAsync("Stage1");
     }
 
 
-    public void openOptions()
-    {
-
-    }
+    
 
     public void openCredits()
     {
-
+        SceneManager.LoadSceneAsync("Credits");
     }
 
     public void quitGame()
     {
         Application.Quit();
     }
-  
+ 
+    public void quitCredits()
+    {
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
 }
